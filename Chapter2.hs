5b052aa1af9ed573533888717de5dee0c65b41bf
Ed Maphis <emaphis85@gmail.com>
2015-10-02 21:24:03 -0400

    Setup project to solve problems form Thomas Craft of Functional Programming 3rd Editon - some files are from his book.

---
+++ Chapter2.hs
@@ -0,0 +1,14 @@
+------------------------------------------------------------------------------
+--
+-- 	Haskell: The Craft of Functional Programming
+-- 	Simon Thompson
+-- 	(c) Addison-Wesley, 2011.
+-- 
+-- 	Chapter 2
+-- 
+-- 	The example script FirstScript.hs is provided separately,
+--      as are the Pictures.hs and PicturesSVG.hs modules.
+--
+------------------------------------------------------------------------------
+
+module Chapter2 where
