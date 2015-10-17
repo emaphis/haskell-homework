98f136d47c62cd51cb0320ef1cf118c23e0a136a
Ed Maphis <emaphis85@gmail.com>
2015-10-16 22:41:02 -0400

    Finished week 1 exercises.

--- fp101/Homework01.hs
+++ fp101/Homework01.hs
@@ -18,7 +18,7 @@ sum2 (x:xs) = x + sum xs
 -- Exerciese 1.7
 
 -- Ex (4)
--- 1. Give another possible calculation for the result of double (double 2)
+-- 1.1. Give another possible calculation for the result of double (double 2)
 -- double x = x + x
 -- double (double 2)
 -- (double 2) + (double 2) {applying outer double}
@@ -28,7 +28,7 @@ sum2 (x:xs) = x + sum xs
 -- 8                       {applying +}
 
 -- Ex (5)
--- 2. Show that sum [x] = x for any number x.
+-- 1.2. Show that sum [x] = x for any number x.
 -- given:
 -- sum []     = 0
 -- sum (x:xs) = x + sum xs
@@ -39,7 +39,7 @@ sum2 (x:xs) = x + sum xs
 -- x           {appply +}
 
 -- Ex (6)
--- 3. Define a function product that produces the product of a list of numbers
+-- 1.3. Define a function product that produces the product of a list of numbers
 -- and show using your definition that product [2, 3, 4] = 24.
 
 product1 []      = 1
@@ -53,22 +53,70 @@ product1 (x:xs)  = x * product1 xs
 -- 24
 
 -- Ex (7)
--- 4. How should the definition of the function qsort be modified so that it
+-- 1.4. How should the definition of the function qsort be modified so that it
 -- produces a reverse sorted version of a list?
-revqsort' []     = []
-revqsort' (x:xs) = revqsort' lrg ++ [x] ++ revqsort' sml
+
+rqsort1 []     = []      -- good - my answer
+rqsort1 (x:xs) = rqsort1 lrg ++ [x] ++ rqsort1 sml
   where sml = [a | a <- xs, a <= x]
         lrg = [b | b <- xs, b > x]
 
+rqsort2 []     = []     -- bad -- just mixing it up
+rqsort2 (x:xs) = reverse (rqsort2 sml ++ [x] ++ rqsort2 lrg)
+  where sml = [a | a <- xs, a <= x]
+        lrg = [b | b <- xs, b > x]
 
+rqsort3 []     = []     -- bad, some of the time
+rqsort3 (x:xs) = rqsort3 lrg ++ rqsort3 sml ++ [x]
+  where sml = [a | a <- xs, a <= x]
+        lrg = [b | b <- xs, b > x]
+
+rqsort4 []     = []     -- bad
+rqsort4 (x:xs)
+  = reverse (rqsort4 sml) ++ [x] ++  reverse (rqsort4 lrg)
+  where sml = [a | a <- xs, a <= x]
+        lrg = [b | b <- xs, b > x]
+
+rqsort5 []     = []     -- good!!
+rqsort5 (x:xs) = rqsort5 lrg ++ [x] ++ rqsort5 sml
+  where lrg = [a | a <- xs, a > x || a ==x]
+        sml = [b | b <- xs, b < x]
+
+rqsort6 []     = []     -- bad - skips equals
+rqsort6 (x:xs) = rqsort6 lrg ++ [x] ++ rqsort6 sml
+  where sml = [a | a <- xs, a < x]
+        lrg = [b | b <- xs, b > x]
+
+rqsort7 []     = []     -- good
+rqsort7 (x:xs)
+  = reverse
+    (reverse (rqsort7 sml) ++ [x] ++ reverse(rqsort7 lrg))
+  where sml = [a | a <- xs, a <= x]
+        lrg = [b | b <- xs, b > x]
+
+rqsort8 []     = []     -- bad
+rqsort8 xs = x : rqsort8 lrg ++  rqsort8 sml
+  where x = maximum xs
+        sml = [a | a <- xs, a < x]
+        lrg = [b | b <- xs, b >= x]
+
+rqsort1' []     = []
+rqsort1' (x:xs) = rqsort1' lrg ++ [x] ++ rqsort1' sml
+  where sml = [a | a <- xs, a <= x]
+        lrg = [b | b <- xs, b > x]
 
 test_revqsort fn =
-  fn []       == []      &&
-  fn [1]      == [1]     &&
-  fn [1,2]    == [2,1]   &&
-  fn [1,2,3]  == [3,2,1] 
+  fn []        == []      &&
+  fn [1]       == [1]     &&
+  fn [1,1]     == [1,1]   &&
+  fn [1,2]     == [2,1]   &&
+  fn [1,2,3]   == [3,2,1] &&
+  fn [1,5,2,8,1] == [8,5,2,1,1]
 
--- 5. What would be the effect of replacing <= by < in the definition of qsort?
+
+-- Ex (8)
+-- 1.5. What would be the effect of replacing <= by < in the
+--    definition of qsort?
 -- Hint: consider the example qsort [2, 2, 3, 1, 1].
 qsortbad []     = []
 qsortbad (x:xs) = qsortbad ys ++ [x] ++ qsortbad zs
@@ -82,26 +130,32 @@ qsortbad (x:xs) = qsortbad ys ++ [x] ++ qsortbad zs
 -- Exerciese  2.6
 
 -- Ex (0)
--- 1. Parenthesise the following arithmetic expressions:
+-- 2.1. Parenthesise the following arithmetic expressions:
 -- (2 ^ 3) ∗ 4
 -- (2 ∗ 3) + (4 ∗ 5)
 -- 2 + (3 ∗ (4 ^ 5))
 
--- 2. Work through the examples from this chapter using Hugs.
+-- 2.2. Work through the examples from this chapter using Hugs.
 
 -- Ex (1)
--- 3. The script below contains three syntactic errors. Correct these errors and
+-- 2.3. The script below contains three syntactic errors. Correct these errors and
 -- then check that your script works properly using Hugs.
+n :: Int
 n = a `div` length xs
-  where
+  where a = 10
+        xs = [1, 2, 3, 4, 5]
+
+m :: Int
+m = a `div` length xs
+ where
     a = 10
     xs = [1, 2, 3, 4, 5]
 
 
--- a and xs don't align, backquotes on div, 'm' should be lower cas
+-- a and xs don't align, backquotes on div, 'm' should be lower case
 
 -- Ex (2)
--- 4. Show how the library function last that selects the last element of a nonempty
+-- 2.4. Show how the library function last that selects the last element of a nonempty
 -- list could be defined in terms of the library functions introduced in
 -- this chapter. Can you think of another possible definition?
 
@@ -137,7 +191,7 @@ test_last fn =
   fn [1,2,3] == 3
 
 -- Ex (3).
--- 5. Show how the library function init that removes the last element from a
+-- 2.5 Show how the library function init that removes the last element from a
 -- non-empty list could similarly be defined in two different ways.
 
 init1 xs = tail (reverse xs)              -- bad, reverses list
@@ -166,5 +220,3 @@ test_init fn =
   fn [1,2,3]   == [1,2] &&
   fn [1,2,3,4] == [1,2,3]
 
-
--- 
