ff8bd7b8b99d63ea3fda07cab68b6e1a1df054b9
Ed Maphis <emaphis85@gmail.com>
2015-11-28 00:55:02 -0500

    Worked on Lecture 9 and Homework  - Declaring Types and Classes

---
+++ fp101/Homework09.hs
@@ -0,0 +1,389 @@
+-- | Homework -- Declaring types and classes.
+{-# LANGUAGE NPlusKPatterns #-}
+
+module Homework09 where
+import Data.List
+import Data.Char
+import Unsafe.Coerce
+-- import Hugs.IOExts (usafeCoerce)
+
+data Nat = Zero
+         | Succ Nat
+         deriving Show
+
+zero,one,two,three :: Nat
+zero  = Zero
+one   = Succ Zero
+two   = Succ one
+three = Succ two
+
+nat2int    :: Nat -> Int
+nat2int Zero  = 0
+nat2int (Succ n) = 1 + nat2int n
+
+int2nat   :: Int -> Nat
+int2nat 0  = Zero
+int2nat n  = Succ (int2nat (n-1))
+
+
+-- Ex (0)  - natToInteger
+
+natToInteger1,natToInteger2,natToInteger3,natToInteger4,
+ natToInteger5,natToInteger6
+  :: Nat -> Int
+
+-- good
+natToInteger1 Zero = 0
+natToInteger1 (Succ n) = natToInteger1 n + 1
+
+-- good
+natToInteger2 (Succ n) = natToInteger2 n + 1
+natToInteger2 Zero = 0
+
+-- bad, - bottom type
+natToInteger3 n = natToInteger3 n
+
+-- good?
+natToInteger4 (Succ n) = 1 + natToInteger4 n
+natToInteger4 Zero = 0
+
+-- bad  - fails output, - constant 1
+natToInteger5 Zero = 1
+natToInteger5 (Succ n ) = (1 + natToInteger5 n) -1
+
+-- good
+natToInteger6 = head . m
+  where m Zero = [0]
+        m (Succ n) = [sum [x | x <- (1 : m n)]]
+
+-- good - counts 'S' ha ha ha
+natToInteger7 :: Nat -> Integer
+natToInteger7 = \ n -> genericLength [c | c <- show n,c == 'S']
+
+-- bad -  doesn't compile, but good if the signature is changed
+natToInteger8 :: Nat -> Int
+natToInteger8 = \ n -> length [c | c <- show n, c == 'S']
+
+
+-- Ex 1  - integerToNat
+
+integerToNat1,integerToNat2,integerToNat5,integerToNat6
+  :: Integer -> Nat
+
+-- good
+integerToNat1 0 = Zero
+integerToNat1 (n+1) = Succ (integerToNat1 n)
+
+-- bad  - bottom type :-)
+integerToNat2 0 = Succ Zero
+integerToNat2 n = (Succ (integerToNat2 n))
+
+-- bad, doesn't type check
+integerToNat3 :: Show a => a -> Integer
+integerToNat3 n
+  = product [(unsafeCoerce c) :: Integer | c <- show n]
+
+-- bad, do not run
+-- integerToNat4 n  = integerToNat4 n
+
+-- good
+integerToNat5 (n+1) = Succ (integerToNat5 n)
+integerToNat5 0 = Zero
+
+-- good
+integerToNat6 (n+1) = let m = integerToNat6 n in Succ m
+integerToNat6 0 = Zero
+
+-- bad  - won't compile
+integerToNat7 :: Integer -> Integer
+integerToNat7 = head . m
+  where {
+        ; m 0 = [0]
+        ; m (n + 1) = [sum [x | x <- (1 : m n)]]
+        }
+
+-- bad  - won't compile
+--integerToNat8 :: Integer -> Nat
+--integerToNat8 = \ n -> genericLength [c | c <- show n, isDigit c]
+
+
+-- Ex (2)  -- add
+
+add1,add2,add3,add4,add5,add6,add7,add8
+  :: Nat -> Nat -> Nat
+
+-- good
+add1 Zero n = n
+add1 (Succ m) n = Succ (add1 n m)
+
+-- good
+add2 (Succ m) n = Succ (add2 n m)
+add2 Zero n = n
+
+-- bad  - wrong result
+add3 Zero n = Zero
+add3 (Succ m) n = Succ (add3 m n)
+
+-- bad  - wrong result
+add4 (Succ m) n = Succ (add4 m n)
+add4 Zero n = Zero
+
+-- bad  - wrong result
+add5 n Zero = Zero
+add5 n (Succ m) = Succ (add5 n m)
+
+-- bad  - wrong result
+add6 n (Succ m) = Succ (add6 n m)
+add6 n Zero = Zero
+
+-- good
+add7 n Zero = n
+add7 n (Succ m) = Succ (add7 m n)
+
+-- good
+add8 n (Succ m) = Succ (add8 m n)
+add8 n Zero = n
+
+testAdd fn m n =
+  nat2int (fn m n) == nat2int m + nat2int n
+
+
+-- Ex (3)  - mult
+
+mult1,mult2,mult3
+  :: Nat -> Nat -> Nat
+
+-- bad  - wrong result
+mult1 Zero Zero = Zero
+mult1 m (Succ n) = add1 m (mult1 m n)
+
+-- good
+mult2 m Zero = Zero
+mult2 m (Succ n) = add1 m (mult2 m n)
+
+-- bad
+mult3 m Zero = Zero
+mult3 m (Succ n) = add1 n (mult3 m n)
+
+testMult fn m n =
+  nat2int (fn m n) == nat2int m * nat2int n
+
+
+-- Ex (4)
+
+data Tree = Leaf Integer
+          | Node Tree Integer Tree
+
+tree4 = Node (Node (Leaf 1) 3 (Leaf 4))
+             5
+             (Node (Leaf 6) 7 (Leaf 9))
+
+occurs1,occurs2,occurs4,occurs5,occurs6
+  :: Integer -> Tree -> Bool
+
+-- good
+occurs1 m (Leaf n) = m == n
+occurs1 m (Node l n r)
+  = case compare m n of
+       LT -> occurs1 m l
+       EQ -> True
+       GT -> occurs1 m r
+
+-- bad  - wrong result
+occurs2 m (Leaf n) = m == n
+occurs2 m (Node l n r)
+  = case compare m n of
+       LT -> occurs2 m r
+       EQ -> True
+       GT -> occurs2 m l
+
+-- bad  - won't compile
+-- occurs3 m (Leaf n) = compare m n
+-- occurs3 m (Node l n r)
+--   = case compare m n of
+--        LT -> occurs3 m l
+--        EQ -> True
+--        GT -> occurs3 m r
+
+-- bad
+occurs4 m (Leaf n) = m == n
+occurs4 m (Node l n r)
+  = case compare m n of
+       LT -> occurs4 m l
+       EQ -> False
+       GT -> occurs4 m r
+
+-- good
+occurs5 m (Leaf n) = m == n
+occurs5 m (Node l n r)
+  | m == n = True
+  | m < n =  occurs5 m l
+  | otherwise = occurs5 m r
+
+-- bad  - wrong results
+occurs6 m (Leaf n) = m == n
+occurs6 m (Node l n r)
+  | m == n = True
+  | m > n =  occurs6 m l
+  | otherwise = occurs6 m r
+
+-- bad  - won't compile
+-- occurs7 m n = m == n
+-- occurs7 m (Node l n r)
+--   | m == n = True
+--   | m < n =  occurs7 m l
+--   | otherwise = occurs7 m r
+
+-- bad - won't compile
+-- occurs8 m n = m == n
+-- occurs8 m (Node l n r)
+--   | m == n = False
+--   | m < n =  occurs8 m r
+--   | otherwise = occurs8 m l
+
+
+-- Ex (5)  balanced tree
+
+data Tree5 = Leaf5 Integer
+           | Node5 Tree5 Tree5
+             deriving (Show)
+
+bin5 = Node5 (Node5 (Leaf5 1) (Leaf5 4))
+             (Node5 (Leaf5 6) (Leaf5 9))
+
+bin6 = Node5 (Node5 (Leaf5 1) (Leaf5 4))
+             (Node5 (Leaf5 9) (Leaf5 6))
+
+
+balanced1, balanced4
+  :: Tree5 -> Bool
+leaves1,leaves4
+  :: Tree5 -> Integer
+
+-- bad
+leaves1 (Leaf5 x) = x
+leaves1 (Node5 l r) = leaves1 l + leaves1 r
+balanced1 (Leaf5 _) = True
+balanced1 (Node5 l r)
+  = abs (leaves1 l - leaves1 r) <= 1 || balanced1 l || balanced1 r
+
+-- won't compile
+-- leaves2 (Leaf5 _) = True
+-- leaves2 (Node5 l r) = leaves2 l + leaves2 r
+-- balanced2 (Leaf5 _) = True
+-- balanced2 (Node5 l r) = abs (leaves2 l - leaves2 r) <= 1
+
+-- won't compile
+-- leaves3 (Leaf5 _) = True
+-- leaves3 (Node5 l r) = leaves3 l + leaves3 r
+-- balanced3 (Leaf5 _) = True
+-- balanced3 (Node5 l r) = abs (leaves3 l - leaves3 r) <= 1
+
+-- good
+leaves4 (Leaf5 _) = 1
+leaves4 (Node5 l r) = leaves4 l + leaves4 r
+balanced4 (Leaf5 _) = True
+balanced4 (Node5 l r)
+  = abs (leaves4 l - leaves4 r) <= 1 && balanced4 l && balanced4 r
+
+
+-- Ex (6) - balance
+halve1 :: [Integer] -> ([Integer],[Integer])
+balance1 :: [Integer] -> Tree5
+
+-- good
+halve1 xs = splitAt (length xs `div` 2) xs
+balance1 [x] = Leaf5 x
+balance1 xs = Node5 (balance1 ys) (balance1 zs)
+  where (ys,zs) = halve1 xs
+
+lst = [1,4,5,6,9]
+
+treea = Node5 (Node5 (Leaf5 1) (Leaf5 4))
+              (Node5 (Leaf5 5)
+                     (Node5 (Leaf5 6) (Leaf5 9)))
+
+-- bad - won't compile
+-- halve2 xs = splitAt (length xs / 2) xs
+-- balance2 [x] = Leaf5 x
+-- balance2 xs = Node5 (balance2 ys) (balance2 zs)
+--   where (ys,zs) = halve2 xs
+
+-- bad - won't compile
+-- halve3 xs = splitAt (length xs `div` 2) xs
+-- balance3 [x] = Leaf5 x
+-- balance3 xs = Node5 ys zs
+--   where (ys,zs) = balance3 (halve3 xs)
+
+-- bad - won't compile
+-- halve4 xs = splitAt (length xs `div` 2) xs
+-- balance4 x = Leaf5 x
+-- balance4 xs = Node5 (balance4 ys) (balance4 zs)
+--   where (ys,zs) = halve4 xs
+
+
+-- Ex (7)
+
+data Expr' =  Add Expr' Expr' | Val Int
+
+exp7 = Add (Val 1) (Val 2)
+
+
+-- Ex (8)
+
+data Tree8 = Leaf8 Int | Node8 Tree8 Tree8
+
+exp8 = Node8 (Leaf8 1) (Leaf8 2)
+
+
+-- Ex (9)  Maybe Monad
+
+class Monad1 m where
+    return :: a -> m a
+    (>>=) :: m a -> (a -> m b) -> m b
+--    (>>) :: m a -> m b -> m b
+--    x >> y = x >>= \_ -> y
+--    fail :: String -> m a
+--    fail msg = error msg
+
+instance Monad1 Maybe where
+  return x = Just x
+  Nothing >>= _ = Nothing
+  (Just x) >>= f = f x
+
+
+-- Ex (10)  list monad
+
+instance Monad1 [] where
+  return x = [x]
+  xs >>= f = concat (map f xs)
+
+
+-- Ex (11)  Monoid
+
+class Monoid1 a where
+  mempty :: a
+  (<>) :: a -> a -> a
+
+instance Monoid1 [a] where
+  mempty = []
+  (<>) = (++)
+
+
+-- Ex (12)  Functor
+
+class Functor1 f where
+  fmap :: (a -> b) -> f a -> f b
+
+instance Functor1 Maybe where
+  fmap _ Nothing = Nothing
+  fmap f (Just a) = Just (f a)
+
+
+-- Ex (13) Foldable
+
+class (Functor f) => Foldable f where
+  fold :: (Monoid1 m) => f m -> m
+
+instance Foldable [] where
+  fold  = foldr (<>) mempty
---
+++ fp101/Lecture09.hs
@@ -0,0 +1,190 @@
+-- | Lecture 9  -- Declaring Types and Classes
+-- | Chapter 10
+
+module Lecture09 where
+
+
+-- 10.1  Types and declarations
+
+-- a new name for an old type
+type String1 = [Char]
+
+-- can make other type easier to read
+type Pos = (Int,Int)
+
+origin  :: Pos
+origin   = (0,0)
+
+left      :: Pos -> Pos
+left (x,y) = (x-1,y)
+
+-- type can also have parameters
+type Pair a = (a,a)
+
+mult      :: Pair Int -> Int
+mult (m,n) = m*n
+
+copy  :: a -> Pair a
+copy x = (x,x)
+
+-- types can be nested:
+type Trans = Pos -> Pos
+
+left'      :: Trans
+left' (x,y) = (x-1,y)
+
+-- type cannot be recursive:
+--type Tree = (Int,[Tree])
+
+
+-- 10.2  Data declarations
+-- a new type specified by its values.
+data Bool' = False' | True'
+-- True and False are constructors
+
+data Answer  = Yes | No | Unknown
+
+answers     :: [Answer]
+answers      = [Yes,No,Unknown]
+
+flip'        :: Answer -> Answer
+flip' Yes     = No
+flip' No      = Yes
+flip' Unknown = Unknown
+
+-- can be used like built in types
+data Move = Left' | Right' | Up' | Down'
+
+move            :: Move -> Pos -> Pos
+move Left' (x,y)  = (x-1,y)
+move Right' (x,y) = (x+1,y)
+move Up' (x,y)    = (x,y-1)
+move Down' (x,y)  = (x,y+1)
+
+moves         :: [Move] -> Pos -> Pos
+moves [] p     = p
+moves (m:ms) p = moves ms (move m p)
+
+flip      :: Move -> Move
+flip Left'   = Right'
+flip Right'  = Left'
+flip Up'     = Down'
+flip Down'   = Up'
+
+-- constructors can also have arguments
+data Shape = Circle Float
+           | Rect Float Float
+
+square   :: Float -> Shape
+square n  = Rect n n
+
+area     :: Shape -> Float
+area (Circle r) = pi * r*r
+area (Rect x y) = x * y
+
+-- data definitions can also take parameters
+data Maybe' a = Nothing' | Just' a
+
+safediv :: Int -> Int -> Maybe Int
+safediv _ 0  = Nothing
+safediv m n  = Just (m `div` n)
+
+safehead     :: [a] -> Maybe a
+safehead []   = Nothing
+safehead xs   = Just (head xs)
+
+
+-- 10.3  Recursive types
+
+data Nat = Zero | Succ Nat
+
+zero,one,two,three :: Nat
+zero = Zero
+one = Succ Zero
+two = Succ one
+three = Succ two
+
+nat2int    :: Nat -> Int
+nat2int Zero  = 0
+nat2int (Succ n) = 1 + nat2int n
+
+int2nat   :: Int -> Nat
+int2nat 0  = Zero
+int2nat n  = Succ (int2nat (n-1))
+
+add           :: Nat -> Nat -> Nat
+add Zero     n = n
+add (Succ m) n = Succ (add m n)
+
+-- add (Succ (Succ Zero)) (Succ Zero)
+-- Succ (add (Succ Zero) (Succ Zero))
+-- Succ (Succ (add Zero (Succ Zero)))
+-- Succ (Succ (Succ Zero))
+
+
+-- a list definition
+data List a = Nil | Cons a (List a)
+
+len            :: List a -> Int
+len Nil         = 0
+len (Cons _ xs) = 1 + len xs
+
+-- Expressions
+
+data Expr = Val Int
+          | Add Expr Expr
+          | Mul Expr Expr
+
+expr1 = Add (Val 1) (Mul (Val 2) (Val 3))
+
+size          :: Expr -> Int
+size (Val n)   = 1
+size (Add x y) = size x + size y
+size (Mul x y) = size x + size y
+
+eval          :: Expr -> Int
+eval (Val n)   = n
+eval (Add x y) = eval x + eval y
+eval (Mul x y) = eval x * eval y
+
+--eval' = foldl id (+) (*)
+
+
+-- Binary Tree
+data Tree = Leaf Int
+          | Node Tree Int Tree
+
+tree1 = Node (Node (Leaf 1) 3 (Leaf 4))
+             5
+             (Node (Leaf 6) 7 (Leaf 9))
+
+-- search the tree for a given integer
+occurs               :: Int -> Tree -> Bool
+occurs m (Leaf n)     = m==n
+occurs m (Node l n r) = m==n
+                        || occurs m l
+                        || occurs m r
+
+-- flatten a tree into a list
+flatten              :: Tree -> [Int]
+flatten (Leaf n)      = [n]
+flatten (Node l n r)  = flatten l
+                        ++ [n]
+                        ++ flatten r
+-- tree is a search tree if 'flatten' returns a sorted list
+-- occurs for a search tree
+occurs'               :: Int -> Tree -> Bool
+occurs' m (Leaf n)            = m==n
+occurs' m (Node l n r) | m==n = True
+                       | m<n  = occurs' m l
+                       | m>n  = occurs' m r
+
+-- 10.4  Tautology checker
+
+
+
+-- 10.5  Abstract machine
+
+
+
+-- 10.6  Class and instance declarations
